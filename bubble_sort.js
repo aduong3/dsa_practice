a7522cd32f7a91c2edd032ef99426b9392414e60
aduong3 <mythalink@gmail.com>
2025-02-21 13:49:37 -0800

    Practice the three sorting algorithms for long term memory

--- bubble_sort.js
+++ bubble_sort.js
@@ -20,15 +20,14 @@ function basicBubbleSort(arr) {
 
 // console.log(basicBubbleSort([8, 104, 27, 4, 7, 10, 1, 2, 3, 15]));
 // console.log(basicBubbleSort([8, 1, 2, 3, 4, 5, 7, 6]));
-console.log(basicBubbleSort([8, 10, 12, 14, 16, 18, 9, 7, 6, 5, 4, 3, 2, 1]));
+// console.log(basicBubbleSort([8, 10, 12, 14, 16, 18, 9, 7, 6, 5, 4, 3, 2, 1]));
 
 function optimizedBubbleSort(arr) {
   //has a swap check and if they haven't swapped, we break out
-  let count = 0;
+
   for (let i = 0; i < arr.length; i++) {
     let hasSwapped = false;
     for (let j = 0; j < arr.length - i - 1; j++) {
-      console.log(arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -36,12 +35,10 @@ function optimizedBubbleSort(arr) {
         hasSwapped = true;
       }
     }
-    count++;
-    console.log("Pass count:", count);
     if (!hasSwapped) break;
   }
   return arr;
 }
 
 // console.log(optimizedBubbleSort([8, 104, 27, 4, 7, 10, 1, 2, 3, 15]));
-// // console.log(optimizedBubbleSort([8, 1, 2, 3, 4, 5, 7, 6]));
+// console.log(optimizedBubbleSort([8, 1, 2, 3, 4, 5, 7, 6]));
---
+++ insertion_sort.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && currentValue < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentValue;
+  }
+  return arr;
+}
+
+console.log(insertionSort([2, 1, 9, 76, 4]));
